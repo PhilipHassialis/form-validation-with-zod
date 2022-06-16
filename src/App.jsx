c9e39fa2da91b2833c869beae2f5c2448e5eb4f7
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2022-06-17 02:11:03 +0300

    React-select inclusion

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { Container, Form, Button, Row, Col } from "react-bootstrap";
 import { useForm, Controller } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as z from "zod";
+import { useEffect, useState } from "react";
+import Select from "react-select";
 
 const schema = z
   .object({
@@ -10,6 +12,7 @@ const schema = z
     email: z.string().email(),
     password: z.string().min(8),
     confirmPassword: z.string().min(8),
+    country: z.string(),
   })
   .superRefine((data, ctx) => {
     if (data.password !== data.confirmPassword) {
@@ -22,6 +25,13 @@ const schema = z
         code: z.ZodIssueCode.custom,
         message: "Password and Confirm Password must match",
         path: ["confirmPassword"],
+      });
+    }
+    if (data.country === "") {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: "Country is required",
+        path: ["country"],
       });
     }
   });
@@ -39,116 +49,178 @@ function App() {
     console.log(data);
   };
 
+  const [countriesData, setCountriesData] = useState([]);
+
+  useEffect(() => {
+    const getCountries = async () => {
+      const resp = await fetch(
+        "https://restcountries.com/v3.1/all?fields=name,cca2"
+      );
+      const data = await resp.json();
+      setCountriesData(
+        data
+          .map((country) => ({
+            value: country.cca2,
+            label: country.name.common,
+          }))
+          .sort((a, b) => a.label.localeCompare(b.label))
+      );
+    };
+
+    getCountries();
+  }, []);
+
   return (
     <Container>
       <Row>
         <Col>
           <Form onSubmit={handleSubmit(submitForm)}>
-            <Form.Group className="mb-3" controlId="formFirstname">
-              <Form.Label>Firstname</Form.Label>
-              <Controller
-                name="text"
-                control={control}
-                defaultValue=""
-                render={({ field }) => (
-                  <Form.Control
-                    type="text"
-                    placeholder="Firstname"
-                    {...field}
-                    isInvalid={errors.firstName}
-                  />
+            <Row>
+              <Form.Group as={Col} className="mb-3" controlId="formFirstname">
+                <Form.Label>Firstname</Form.Label>
+                <Controller
+                  name="firstName"
+                  control={control}
+                  defaultValue=""
+                  render={({ field }) => (
+                    <Form.Control
+                      type="text"
+                      placeholder="Firstname"
+                      {...field}
+                      isInvalid={errors.firstName}
+                    />
+                  )}
+                />
+                {errors.firstName?.message && (
+                  <div style={{ color: "red", fontSize: "0.8rem" }}>
+                    {errors.firstName.message}
+                  </div>
                 )}
-              />
-              {errors.firstName?.message && (
-                <div style={{ color: "red", fontSize: "0.8rem" }}>
-                  {errors.firstName.message}
-                </div>
-              )}
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formLastname">
-              <Form.Label>Lastname</Form.Label>
-              <Controller
-                name="text"
-                control={control}
-                defaultValue=""
-                render={({ field }) => (
-                  <Form.Control
-                    type="text"
-                    placeholder="Lastname"
-                    {...field}
-                    isInvalid={errors.lastName}
-                  />
+              </Form.Group>
+              <Form.Group as={Col} className="mb-3" controlId="formLastname">
+                <Form.Label>Lastname</Form.Label>
+                <Controller
+                  name="lastName"
+                  control={control}
+                  defaultValue=""
+                  render={({ field }) => (
+                    <Form.Control
+                      type="text"
+                      placeholder="Lastname"
+                      {...field}
+                      isInvalid={errors.lastName}
+                    />
+                  )}
+                />
+                {errors.lastName?.message && (
+                  <div style={{ color: "red", fontSize: "0.8rem" }}>
+                    {errors.lastName.message}
+                  </div>
                 )}
-              />
-              {errors.lastName?.message && (
-                <div style={{ color: "red", fontSize: "0.8rem" }}>
-                  {errors.lastName.message}
-                </div>
-              )}
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>Email address</Form.Label>
-              <Controller
-                name="email"
-                control={control}
-                defaultValue=""
-                render={({ field }) => (
-                  <Form.Control
-                    type="text"
-                    placeholder="Enter email"
-                    {...field}
-                    isInvalid={errors.email}
-                  />
+              </Form.Group>
+            </Row>
+            <Row>
+              <Form.Group as={Col} className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Controller
+                  name="email"
+                  control={control}
+                  defaultValue=""
+                  render={({ field }) => (
+                    <Form.Control
+                      type="text"
+                      placeholder="Enter email"
+                      {...field}
+                      isInvalid={errors.email}
+                    />
+                  )}
+                />
+                {errors.email?.message && (
+                  <div style={{ color: "red", fontSize: "0.8rem" }}>
+                    {errors.email.message}
+                  </div>
                 )}
-              />
-              {errors.email?.message && (
-                <div style={{ color: "red", fontSize: "0.8rem" }}>
-                  {errors.email.message}
-                </div>
-              )}
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-              <Form.Label>Password</Form.Label>
-              <Controller
-                name="password"
-                control={control}
-                defaultValue=""
-                render={({ field }) => (
-                  <Form.Control
-                    type="password"
-                    placeholder="Password"
-                    {...field}
-                    isInvalid={errors.password}
-                  />
+              </Form.Group>
+            </Row>
+            <Row>
+              <Form.Group
+                as={Col}
+                className="mb-3"
+                controlId="formBasicPassword"
+              >
+                <Form.Label>Password</Form.Label>
+                <Controller
+                  name="password"
+                  control={control}
+                  defaultValue=""
+                  render={({ field }) => (
+                    <Form.Control
+                      type="password"
+                      placeholder="Password"
+                      {...field}
+                      isInvalid={errors.password}
+                    />
+                  )}
+                />
+                {errors.password?.message && (
+                  <div style={{ color: "red", fontSize: "0.8rem" }}>
+                    {errors.password.message}
+                  </div>
                 )}
-              />
-              {errors.password?.message && (
-                <div style={{ color: "red", fontSize: "0.8rem" }}>
-                  {errors.password.message}
-                </div>
-              )}
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicConfirmPassword">
-              <Form.Label>Confirm Password</Form.Label>
-              <Controller
-                name="confirmPassword"
-                control={control}
-                defaultValue=""
-                render={({ field }) => (
-                  <Form.Control
-                    type="password"
-                    placeholder="Confirm Password"
-                    {...field}
-                    isInvalid={errors.confirmPassword}
-                  />
+              </Form.Group>
+              <Form.Group
+                as={Col}
+                className="mb-3"
+                controlId="formBasicConfirmPassword"
+              >
+                <Form.Label>Confirm Password</Form.Label>
+                <Controller
+                  name="confirmPassword"
+                  control={control}
+                  defaultValue={""}
+                  render={({ field }) => (
+                    <Form.Control
+                      type="password"
+                      placeholder="Confirm Password"
+                      {...field}
+                      isInvalid={errors.confirmPassword}
+                    />
+                  )}
+                />
+                {errors.confirmPassword?.message && (
+                  <div style={{ color: "red", fontSize: "0.8rem" }}>
+                    {errors.confirmPassword.message}
+                  </div>
                 )}
-              />
-              {errors.confirmPassword?.message && (
-                <div style={{ color: "red", fontSize: "0.8rem" }}>
-                  {errors.confirmPassword.message}
-                </div>
-              )}
-            </Form.Group>
+              </Form.Group>
+            </Row>
+            {countriesData.length > 0 && (
+              <Row className="mb-3">
+                <Form.Group as={Col} controlId="formBasicCountry">
+                  <Form.Label>Country of birth</Form.Label>
+                  <Controller
+                    name="country"
+                    control={control}
+                    defaultValue=""
+                    render={({ field }) => (
+                      <Select
+                        options={countriesData}
+                        value={countriesData.find(
+                          (c) => c.value === field.value
+                        )}
+                        ref={field.ref}
+                        onChange={(val) => field.onChange(val.value)}
+                      />
+                    )}
+                  />
+                  {errors.country?.message && (
+                    <div style={{ color: "red", fontSize: "0.8rem" }}>
+                      {errors.country.message}
+                    </div>
+                  )}
+                </Form.Group>
+              </Row>
+            )}
             <Button variant="primary" type="submit">
               Submit
             </Button>

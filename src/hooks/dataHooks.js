ff018be9b1e5b5a9f2b24ef9ebd656624125e3c9
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2022-06-29 08:56:32 +0300

    useSWR introduction

--- src/hooks/dataHooks.js
+++ src/hooks/dataHooks.js
@@ -1,26 +1,31 @@
-import { useState, useEffect } from "react";
+import useSWR from "swr";
 
-export const getCountriesData = () => {
-  const [countriesData, setCountriesData] = useState([]);
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
-  useEffect(() => {
-    const getCountries = async () => {
-      const resp = await fetch(
-        "https://restcountries.com/v3.1/all?fields=name,cca2"
-      );
-      const data = await resp.json();
-      setCountriesData(
-        data
-          .map((country) => ({
-            value: country.cca2,
-            label: country.name.common,
-          }))
-          .sort((a, b) => a.label.localeCompare(b.label))
-      );
-    };
+export const useCountriesData = () => {
+  const { data, error } = useSWR(
+    "https://restcountries.com/v3.1/all?fields=name,cca2",
+    fetcher
+  );
 
-    getCountries();
-  }, []);
+  return {
+    countriesData: data
+      ?.map((country) => ({
+        value: country.cca2,
+        label: country.name.common,
+      }))
+      .sort((a, b) => a.label.localeCompare(b.label)),
+    countriesLoading: !error && !data,
+    countriesError: error,
+  };
+};
 
-  return countriesData;
+export const useProductsData = () => {
+  const { data, error } = useSWR("https://dummyjson.com/products", fetcher);
+
+  return {
+    productsData: data,
+    productsLoading: !error && !data,
+    productsError: error,
+  };
 };

209060ce03a34d4de7d58b93a5d3860b05f8e919
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2022-07-08 03:48:05 +0300

    Initial routing concepts

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,27 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import EmployeeProductSelection from "./components/Forms/EmployeeProductSelection";
+import EmployeeRegistration from "./components/Forms/EmployeeRegistration";
+import InvalidRoute from "./components/Forms/InvalidRoute";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route
+            path="/productSelection"
+            element={<EmployeeProductSelection />}
+          />
+          <Route
+            path="/employeeRegistration"
+            element={<EmployeeRegistration />}
+          />
+          <Route path="*" element={<InvalidRoute />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
